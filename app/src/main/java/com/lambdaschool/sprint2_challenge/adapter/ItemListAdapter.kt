419de7cb36ed20f037d1a08c8f92a6a33f0ba029
Deran Decker <me@derandecker.com>
2019-08-19 10:28:29 -1000

    Add RecyclerView animation to CardViews

--- app/src/main/java/com/lambdaschool/sprint2_challenge/adapter/ItemListAdapter.kt
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/adapter/ItemListAdapter.kt
@@ -5,10 +5,13 @@ import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.content.ContextCompat.getDrawable
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.Switch
 import android.widget.TextView
@@ -20,7 +23,10 @@ import kotlinx.android.synthetic.main.recycler_cardview_layout.view.*
 class ItemListAdapter(val imageList: IntArray, val itemNameList: Array<String>)
     : RecyclerView.Adapter<ItemListAdapter.ViewHolder>() {
 
+    private var lastPosition = -1
+
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val card: CardView = view.cardview
         val image: ImageView = view.iv_shopping_item
         val itemName: TextView = view.tv_shopping_item
         val toggle: Switch = view.switch_toggle_item
@@ -54,7 +60,7 @@ class ItemListAdapter(val imageList: IntArray, val itemNameList: Array<String>)
 
         }
 
-        holder.itemName.setOnClickListener {view ->
+        holder.itemName.setOnClickListener { view ->
             val intent = Intent(view.context, ItemDetailsActivity::class.java)
 
             val optionsBundle: Bundle = ActivityOptions.makeSceneTransitionAnimation(view.context as Activity,
@@ -64,6 +70,16 @@ class ItemListAdapter(val imageList: IntArray, val itemNameList: Array<String>)
             view.context.startActivity(intent, optionsBundle)
         }
 
+        fun setEnterAnimation(viewToAnimate: View, position: Int) {
+            if (position > lastPosition) {
+                val animation: Animation = AnimationUtils.loadAnimation(viewToAnimate.context,
+                        android.R.anim.slide_in_left)
+                viewToAnimate.startAnimation(animation)
+                lastPosition = position
+            }
+        }
+
+        setEnterAnimation(holder.card, position)
 
     }
 
